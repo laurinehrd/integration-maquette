50fdd20cad67f1b685de5a67e9c629e4492edbea
laurinehrd <laurine.hrd@hotmail.fr>
2020-05-19 14:33:53 +0200

    integration maquette

---
+++ index.php
@@ -0,0 +1,181 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <title>Intégration maquette</title>
+  </head>
+  <body>
+
+    <!-- Toolbar -->
+    <!--Main Navigation-->
+    <header>
+
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Offers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    </header>
+    <!--Main Navigation-->
+
+
+    <!-- head -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="banner">
+          <img class="logo" src="images/logo.png" alt="logo"/>
+        </div>
+      </div>
+    </div>
+
+    <!-- Enjoy breakfast -->
+    <div class="enjoy">
+      <h1>Enjoy<br> breakfast</h1>
+      <p> Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit. Vivamus lacinia odio vitae. </p>
+      <button class="bouton" type="button" >More info</button>
+    </div>
+
+    <!-- I love coffee -->
+    <div class"=container">
+      <div class="row">
+        <div class="col-md-8">
+          <img src="images/coffee.png" alt"coffee"/>
+        </div>
+        <div class="col-md-4 cafe">
+          <h1 class="coffee">I love coffee</h1>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- See our menu -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <img src="images/titre-menu.png" alt="Titre menu"/>
+          <h1 class="titremenu">See our menu</h1>
+        </div>
+      </div>
+    </div>
+
+    <!-- menu -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h2 class="titrecoffee">Coffee</h2>
+          <img class="miniimg" src="images/menu-coffee.png" alt="tasse de café"/>
+          <div class="contour">
+            <p class="menuname">Coffee name here<br>Coffee name here<br>Coffee name here<br>Coffee name here<br>Coffee name here</p>
+            <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h2 class="titredrink">Drink</h2>
+          <img class="miniimg" src="images/menu-drink.png" alt="smoothies"/>
+          <div class="contour">
+            <p class="menuname">Drink name here<br>Drink name here<br>Drink name here<br>Drink name here<br>Drink name here</p>
+            <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h2 class="titresnack">Snack</h2>
+          <img class="miniimg" src="images/menu-snack.png" alt="vienoiseries"/>
+          <div class="contour">
+            <p class="menuname">Snack name here<br>Snack name here<br>Snack name here<br>Snack name here<br>Snack name here</p>
+            <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h2 class="titresandwich">Sandwich</h2>
+          <img class="miniimg" src="images/menu-sandwich.png" alt="sandwich"/>
+          <div class="contour">
+            <p class="menuname">Sandwich name here<br>Sandwich name here<br>Sandwich name here<br>Sandwich name here<br>Sandwich name here</p>
+            <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tasty meals -->
+    <div class="container-fluid fond">
+      <div class="row">
+        <div class="col-md-12">
+          <img class="sand" src="images/tastymeals.png" alt="sandwich"/>
+          <h1 class="htasty">Tasty <br>meals</h1>
+          <p class="ptasty">“Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, sed do<br> eiusmod tempor incididunt ut <br>labore et dolore magna aliqua.”</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Delicious smoothies -->
+    <div class="container-fluid machin">
+      <div class="row">
+        <div class="col-md-5 delicious">
+          <h1 class="tdelicious">Delicious <br>smoothies</h1>
+          <p class="pdelicious">“Lorem ipsum dolor sit <br>amet, consectetur <br>adipiscing elit, sed do <br>eiusmod tempor incididunt <br>ut labore et dolore magna <br>aliqua.”</p>
+          <button class="bouton bdelicious" type="button" >More info</button>
+        </div>
+        <div class="col-md-7">
+          <img class="imgsmooth"src="images/delicioussmoothies.png" alt="smoothies"/>
+        </div>
+      </div>
+    </div>
+
+    <!-- Offers -->
+    <div class="container machin">
+      <div class="row text-center">
+        <div class="col-md-12">
+          <h3 class="get">Get our Latest Offers!</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus <br>lacinia odio vitae vestibulum vestibulum. </p>
+          <form class="form-inline">
+            <div class="input-group">
+              <input type="text" class="form-control" value="Your email">
+              <span class="input-group-btn">
+                <button class="btn btn-rounded" type="button">Subscribe</button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+
+
+
+
+
+
+  </body>
+</html>
