09bd18ab550ba0cffb9560d1e269fa25b1a6581c
laurinehrd <laurine.hrd@hotmail.fr>
2020-05-25 09:30:33 +0200

    update css

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- Font Awesome -->
@@ -12,6 +13,7 @@
 <!-- Material Design Bootstrap -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.18.0/css/mdb.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+
     <title>Intégration maquette</title>
   </head>
   <body>
@@ -27,8 +29,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mx-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">About</a>
@@ -61,30 +63,31 @@
 
     <!-- Enjoy breakfast -->
     <div class="enjoy">
-      <h1>Enjoy<br> breakfast</h1>
-      <p> Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit. Vivamus lacinia odio vitae. </p>
-      <button class="bouton" type="button" >More info</button>
+      <h1 class="henjoy">Enjoy<br> breakfast</h1>
+      <p class="penjoy"> Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit. Vivamus lacinia odio vitae. </p>
+      <button class="benjoy" type="button" >More info</button>
     </div>
 
     <!-- I love coffee -->
-    <div class"=container">
+    <div class="container lovecoffee">
       <div class="row">
-        <div class="col-md-8">
-          <img src="images/coffee.png" alt"coffee"/>
+        <div class="col-md-7">
+          <img class="imglovecoffee" src="images/coffee.png" alt"coffee"/>
+          <p class="new"></p>
         </div>
-        <div class="col-md-4 cafe">
-          <h1 class="coffee">I love coffee</h1>
+        <div class="col-md-5 cafe">
+          <h1 class="coffee">I love <br>coffee</h1>
         </div>
       </div>
     </div>
 
 
     <!-- See our menu -->
-    <div class="container-fluid">
-      <div class="row">
+    <div class="container-fluid ourmenu">
+      <div class="row text-center">
         <div class="col-md-12">
-          <img src="images/titre-menu.png" alt="Titre menu"/>
-          <h1 class="titremenu">See our menu</h1>
+          <img class="imgbanmenu" src="images/titre-menu.png" alt="Titre menu"/>
+          <h1 class="titremenu">See our <br>menu</h1>
         </div>
       </div>
     </div>
@@ -93,33 +96,34 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <h2 class="titrecoffee">Coffee</h2>
+
           <img class="miniimg" src="images/menu-coffee.png" alt="tasse de café"/>
           <div class="contour">
+            <h2 class="titrecoffee">Coffee</h2>
             <p class="menuname">Coffee name here<br>Coffee name here<br>Coffee name here<br>Coffee name here<br>Coffee name here</p>
             <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
           </div>
         </div>
         <div class="col-md-6">
-          <h2 class="titredrink">Drink</h2>
           <img class="miniimg" src="images/menu-drink.png" alt="smoothies"/>
           <div class="contour">
+            <h2 class="titrecoffee">Drink</h2>
             <p class="menuname">Drink name here<br>Drink name here<br>Drink name here<br>Drink name here<br>Drink name here</p>
             <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
           </div>
         </div>
         <div class="col-md-6">
-          <h2 class="titresnack">Snack</h2>
           <img class="miniimg" src="images/menu-snack.png" alt="vienoiseries"/>
           <div class="contour">
+            <h2 class="titrecoffee">Snack</h2>
             <p class="menuname">Snack name here<br>Snack name here<br>Snack name here<br>Snack name here<br>Snack name here</p>
             <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
           </div>
         </div>
         <div class="col-md-6">
-          <h2 class="titresandwich">Sandwich</h2>
           <img class="miniimg" src="images/menu-sandwich.png" alt="sandwich"/>
           <div class="contour">
+            <h2 class="titrecoffee">Sandwich</h2>
             <p class="menuname">Sandwich name here<br>Sandwich name here<br>Sandwich name here<br>Sandwich name here<br>Sandwich name here</p>
             <p class="price">$10<br>$10<br>$10<br>$10<br>$10</p>
           </div>
@@ -129,9 +133,10 @@
 
     <!-- Tasty meals -->
     <div class="container-fluid fond">
-      <div class="row">
-        <div class="col-md-12">
-          <img class="sand" src="images/tastymeals.png" alt="sandwich"/>
+      <div class="row tastymealsimg h-100 align-items-center" >
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6 ">
           <h1 class="htasty">Tasty <br>meals</h1>
           <p class="ptasty">“Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, sed do<br> eiusmod tempor incididunt ut <br>labore et dolore magna aliqua.”</p>
         </div>
@@ -158,23 +163,35 @@
         <div class="col-md-12">
           <h3 class="get">Get our Latest Offers!</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus <br>lacinia odio vitae vestibulum vestibulum. </p>
-          <form class="form-inline">
-            <div class="input-group">
-              <input type="text" class="form-control" value="Your email">
-              <span class="input-group-btn">
-                <button class="btn btn-rounded" type="button">Subscribe</button>
-              </span>
-            </div>
-          </form>
+          <input class="btnemail" type="text" value="Your Email"/>
+          <input class="btnsub" type="submit" value="Subscribe"/>
         </div>
       </div>
     </div>
 
     <!-- Footer -->
-
-
-
-
+    <div class="container-fluid fondfooter">
+      <div class="row">
+        <div class="col-md-5">
+          <img class="logofooter" src="images/logo.png" alt="logo"/>
+          <p class="parag"> Lorem ipsum dolor sit amet, consectetur adipi-<br>scing elit. Aliquam at dignissim nunc, id maxi-<br>mus ex. Etiam nec dignissim elit, at dignissim <br>enim. </p>
+        </div>
+        <div class="col-md-offset-1">
+        </div>
+        <div class="col-md-2 about">
+          <h4 class="title">About</h4>
+          <p> History <br>Our Team <br>Brand Guidelines <br>Terms & Condition<br>Privacy Policy</p>
+        </div>
+        <div class="col-md-2 services">
+          <h4 class="title">Services</h4>
+          <p>How to Order<br>Our Product<br>Offers<br>Promo<br>Payment Method</p>
+        </div>
+        <div class="col-md-2 other">
+          <h4 class="title">Other</h4>
+          <p>Contact Us<br>Help</p>
+        </div>
+      </div>
+    </div>
 
 
   </body>
